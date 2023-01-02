8f4f9391a7a0a7e48b17ec3bfbb0ecb864e4008e
Carsten Bauer <carsten.bauer@uni-paderborn.de>
2023-01-02 13:30:43 +0100

    init

---
+++ src/ScoreP.jl
@@ -0,0 +1,5 @@
+module ScoreP
+
+greet() = print("Hello World!")
+
+end # module ScoreP
---
+++ test/runtests.jl
@@ -0,0 +1,8 @@
+using TestItemRunner
+
+# Threads.nthreads() ≥ 4 ||
+#     error("At least 4 Julia threads necessary. Forgot to set `JULIA_NUM_THREADS`?")
+
+@run_package_tests
+
+# @testitem "utility" begin include("utility_test.jl") end
